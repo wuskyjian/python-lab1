29e10cbd2edb384fbb721e3051e515825b0b7ee3
WUTIANJIAN <wuskyjian@gmail.com>
2018-03-19 15:41:15 +0100

    lab 1

---
+++ lab1.py
@@ -0,0 +1,16 @@
+#  Exercise 1
+a = 20
+b = 30
+
+print a+b
+
+#  Exercise 2
+string = 'spring'
+length = len(string)
+
+print string[0:2] + string[(length - 2):]
+
+#  Exercise 3
+state =
+
+while(state)
