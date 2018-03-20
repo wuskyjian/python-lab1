3475f372cab81be3990b1a3c70448f4389e3a481
WUTIANJIAN <wuskyjian@gmail.com>
2018-03-20 10:15:51 +0100

    python 3.6.4 version

--- lab1.py
+++ lab1.py
@@ -2,15 +2,12 @@
 a = 20
 b = 30
 
-print a+b
+print(a + b)
 
 #  Exercise 2
 string = 'spring'
 length = len(string)
 
-print string[0:2] + string[(length - 2):]
+print(string[0:2] + string[(length - 2):])
 
 #  Exercise 3
-state =
-
-while(state)
